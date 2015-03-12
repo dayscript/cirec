b3d0280529b594c2960c4390b1f0c1eabbff4b16
Desarrollo2 <jmeneses@dayscript.com>
2015-03-12 18:04:08 -0500

    Overriding the width of Sidebar-theme-regions

--- sites/all/themes/cirec/template.php
+++ sites/all/themes/cirec/template.php
@@ -10,6 +10,37 @@ function cirec_preprocess_html(&$variables) {
  * Implements template_preprocess_page.
  */
 function cirec_preprocess_page(&$variables) {
+
+    // Convenience variables.
+    if (!empty($variables['page']['sidebar_first'])) {
+      $left = $variables['page']['sidebar_first'];
+    }
+
+    if (!empty($variables['page']['sidebar_second'])) {
+      $right = $variables['page']['sidebar_second'];
+    }
+
+    // Dynamic sidebars.
+    if (!empty($left) && !empty($right)) {
+      $variables['main_grid'] = 'medium-8 medium-push-8';
+      $variables['sidebar_first_grid'] = 'medium-8 medium-pull-8';
+      $variables['sidebar_sec_grid'] = 'medium-8';
+    }
+    elseif (empty($left) && !empty($right)) {
+      $variables['main_grid'] = 'medium-16';
+      $variables['sidebar_first_grid'] = '';
+      $variables['sidebar_sec_grid'] = 'medium-8';
+    }
+    elseif (!empty($left) && empty($right)) {
+      $variables['main_grid'] = 'medium-16 medium-push-8';
+      $variables['sidebar_first_grid'] = 'medium-8 medium-pull-16';
+      $variables['sidebar_sec_grid'] = '';
+    }
+    else {
+      $variables['main_grid'] = '';
+      $variables['sidebar_first_grid'] = '';
+      $variables['sidebar_sec_grid'] = '';
+    }
 }
 
 /**
@@ -17,3 +48,4 @@ function cirec_preprocess_page(&$variables) {
  */
 function cirec_preprocess_node(&$variables) {
 }
+
