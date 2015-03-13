8a45e5c171de698dd5b0c01034ac62e6e4bd02b2
Desarrollo2 <jmeneses@dayscript.com>
2015-03-13 14:52:55 -0500

    Adding window scroll when the contact link is clicked.

--- sites/all/themes/cirec/js/_cirec.js
+++ sites/all/themes/cirec/js/_cirec.js
@@ -1,1 +1,25 @@
 /* Implement customer javascript here */
+(function($) {
+    Drupal.behaviors.cirec = {
+        attach: function(context, settings){
+
+            // Scroll al hacer click en el enlace de contacto
+            if ( $('.form-contacto').length !== 0 ) {
+                $('.contacto-footer').on('click', { elem: '.form-contacto' }, scrollToElem );
+            }
+
+
+
+
+            
+        }
+    }; //-- end Drupal.behaviors
+
+    function scrollToElem (e) {
+        e.preventDefault();
+        var body = jQuery("html, body"),
+            elHeight = $(e.data.elem).offset().top;
+        body.animate({ scrollTop:elHeight }, '800', 'swing');
+    }
+
+})(jQuery);
