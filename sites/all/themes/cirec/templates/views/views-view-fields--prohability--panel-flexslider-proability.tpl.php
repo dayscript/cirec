72f22bc8d7437253e11fd5013aed6c64d2899601
aacevedoday <aacevedo@dayscript.com>
2015-07-01 10:27:11 -0500

    Proability

--- sites/all/themes/cirec/templates/views/views-view-fields--prohability--panel-flexslider-proability.tpl.php
+++ sites/all/themes/cirec/templates/views/views-view-fields--prohability--panel-flexslider-proability.tpl.php
@@ -1,4 +1,33 @@
 <?php dpm($fields)?>
 
 
+<div class="view-header">
+  <h2>PROTESIS Y ORTESIS</h2>
+  <hr>
+</div>
 
+<div class="view-content">
+      <div id="flexslider-3" class="flexslider flexslider-processed">
+          <div class="flex-viewport" style="overflow: hidden; position: relative;">
+            <ul class="slides" style="width: 800%; -webkit-transition-duration: 0s; transition-duration: 0s; -webkit-transform: translate3d(-1140px, 0px, 0px); transform: translate3d(-1140px, 0px, 0px);">
+              <li class="clone" aria-hidden="true" style="width: 1140px; float: left; display: block;">
+                <div class="views-field views-field-field-subtitulo left-d">
+                  40 años
+                </div>
+                <div class="views-field views-field-body left-d">
+                     <?php print render($fields['field_subtitulo']->content);?>
+                </div>
+
+                <div class="views-field views-field-field-imagenes rigth-d">
+                  <img typeof="foaf:Image" src="http://cirec.demodayscript.com/sites/default/files/styles/image_340_200_/public/imagefield_KJ6ljt.gif?itok=SuJZbkwL" width="340" height="200" alt="Dolore metuo populus. Caecus nostrud nunc plaga populus quia refoveo utinam valde." title="Enim paratus roto. Aliquam commoveo exerci tum vicis." draggable="false">
+                </div>
+              </li>
+          </ul>
+        </div>
+        <ol class="flex-control-nav flex-control-paging">
+          <li>
+            <a class="flex-active">1</a>
+          </li>
+        </ol>
+    </div>
+</div>
